7ddcb7081c0d52e256b39e2b5f4ea2e4f6898f19
Cydius <oczadly@online.de>
2015-06-15 19:31:24 +0200

    bubblesort
    added bubblesport

--- testing/sort.c
+++ testing/sort.c
@@ -2,11 +2,23 @@
 #include <stdio.h>
 
 void bubblesort(int *numbers) {
-	// echo bla
+	int i,j;
+	
+	for(i=0;i<sizeof(numbers)-1;i++) {
+		for(j=0;j<sizeof(numbers)-i-1;j++) {
+			if(numbers[i]>numbers[j+1]) {
+				int tmp = numbers[j];
+				numbers[j] = numbers[j+1];
+				numbers[j+1] = tmp;
+			}
+		}
+	}
 }
 
 int main() {
 	int numbers[100];
 	
+	bubblesort(numbers);
 	
+	exit(0);
 }
