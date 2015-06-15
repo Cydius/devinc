35538c64245e55260f9e2defa377780c192d7eb4
Cydius <oczadly@online.de>
2015-06-15 19:16:15 +0200

    clear code
    removed bad code

--- testing/devC.c
+++ testing/devC.c
@@ -10,27 +10,7 @@ int getStrSize(char *ptr) {
 	return size;
 }
 
-// trying to reverse text of pointer
-char *reverse(char *stext) {
-	int textSize = getStrSize(stext);
-	int i;
-	char reverse[textSize];
-	char *ptrReturn;
-
-	// goto last char
-	while(*stext != '\0') {
-		stext++;
-	}
-	stext--;
-	
-	// fill array with chars from pointer
-	for(i=0;i<textSize;i++) {
-		reverse[i] = *stext;
-		stext--;
-	}
-	ptrReturn = reverse;
-	return ptrReturn;
-}
+// was shit!
 
 int main(int argc, char *argv[]) {
 	printf("%s\n",reverse(argv[1]));
